dbb5c5d81a99a69cef05bef5bdb5073f7fc91955
andrew-pyle <axpyle@ualr.edu>
2017-03-17 14:03:55 +0000

    Pythonanywhere file structure

--- plotly-test/plotly-test/app.py
+++ plotly-test/plotly-test/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 
+import json
 import plotly.offline
 import plotly.graph_objs as go
 import pandas as pd
@@ -12,38 +13,19 @@ data = "data.json"
 df = pd.read_json(data)
 df.index += 1
 
-
 # Function Declarations
-def AccountHistogram():
-    ipSeries = df["IP Address"].value_counts()
-    ipFrame = pd.DataFrame({'ipAddress':ipSeries.index, 'login count':ipSeries.values})
+def AccountHistogram(data_filename):
+    #df = pd.read_json(data_filename)
     trace = [
-        go.Bar(
-            x=ipFrame["ipAddress"],
-            y=ipFrame["login count"],
-            width = 1,
-            marker = dict(
-                color='rgb(158,202,225)',
-                line=dict(
-                    color='rgb(8,48,107)',
-                    width=1.5,
-                ),
-                #colorbar = dict(
-                #    thickness = 1,
-                #),
-            ),
-            opacity=0.6,
-        ),
+        go.Histogram(x=df["IP Address"]),
     ]
     layout = go.Layout(
         margin = dict(
             l=10,
             r=0,
             t=0,
-            #b=100,
         ),
     )
-
     accounts = go.Figure(data=trace, layout=layout)
 
     return plotly.offline.plot(
@@ -53,7 +35,7 @@ def AccountHistogram():
         show_link=False,
         )
 
-def LocationHistogram():
+def LocationHistogram(data_filename):
     #df = pd.read_json(data_filename)
     trace = [
         go.Histogram(x=df["City"])
@@ -74,15 +56,31 @@ def LocationHistogram():
         show_link=False,
         )
 
-## URL Routing
+
 @app.route("/")
 def index():
     return render_template(
         "index.html",
-        account_histogram=AccountHistogram(),
-        location_histogram=LocationHistogram(),
+        account_histogram=AccountHistogram(data),
+        location_histogram=LocationHistogram(data),
         data_table=df.to_html(),
         )
+
+@app.route("/hero_thirds")
+def hero_thirds():
+    return render_template("hero_thirds.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route("/number1")
+def number1():
+    return render_template("number1.html")
+
+@app.route("/geo")
+def geo():
+    return render_template("geo.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
