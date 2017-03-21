72dbdeaa73020d096d24a8e3a6835b9019ef0118
andrew-pyle <axpyle@ualr.edu>
2017-03-20 22:18:00 -0500

    Add test marker to Leaflet Map
    Changed the Leaflet CSS stylesheet to the one listed on the Leaflet
    website. and added a test marker to the map. The previous stylesheet
    could not load the file.

--- plotly-test/static/assets/js/leafletMapCustom.js
+++ plotly-test/static/assets/js/leafletMapCustom.js
@@ -4,3 +4,7 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a','b','c'],
 }).addTo(map);
+
+var marker = L.marker([34.7464809, -92.2895947]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
