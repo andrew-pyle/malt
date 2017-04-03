bfb93e0e92452d15823fbcd991622274476b2c0f
andrew-pyle <axpyle@ualr.edu>
2017-04-03 16:18:12 -0500

    Add Login HTML page

--- MALT-development/db_connector.py
+++ MALT-development/db_connector.py
@@ -1,11 +1,38 @@
 import pymysql.cursors
-import pandas as pd
 
-def db_connection():
+class DBConnection
+"""
+MALT's connection to MySQL database using PyMySQL.
+
+Attributes:
+    host:       PyMySQL host
+    user:       PyMySQL user
+    password:   PyMySQL password
+    db:         PyMySQL database Name
+    charset:    PyMySQL character set (='utf8mb4')
+    cursorclass: PyMySQL cursor setting
+    sql:        SQL query to execute
+    query:      query result
+Methods:
+    db_connection()
+"""
+
+def __init__(self, host, user, password, db, charset, cursorclass):
+    self.host = host
+    self.user = user
+    self.password = password
+    self.db = db
+    self.charset = charset
+    self.cursorclass = cursorclass
+    # self.sql = sql
+    # self.query = query
+
+
+def db_connection():  #host='localhost', user='root', password, db, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor):
     # Connect to the database
     connection = pymysql.connect(host='localhost',
                                  user='root',
-                                 password='',
+                                 password='a1boxxit',
                                  db='malttest',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
@@ -22,12 +49,11 @@ def db_connection():
 
         with connection.cursor() as cursor:
             # Read a single record
-            sql = "SELECT * FROM `testtable`"
+            sql = "SELECT `userName`,`userPassword` FROM maltusers WHERE `userName` = 'user1'";
             cursor.execute(sql)
             result = cursor.fetchall()
             return(result)
     finally:
         connection.close()
 
-# df = pd.DataFrame(result)
-# print(df.head())
+#print(db_connection())
