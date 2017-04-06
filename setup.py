15f45e8963c8f414531800a2385927d702a8ac1a
andrew-pyle <axpyle@ualr.edu>
2017-04-05 22:01:53 -0500

    Python Package File Structure
    Changed and renamed the Git repository to reflect a python package
    structure

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='malt',
+      version='0.1',
+      description='Flask Web app for tracking UA Little Rock network data',
+      url='http://github.com/andrew-pyle/MALT.git',
+      license='MIT',)
